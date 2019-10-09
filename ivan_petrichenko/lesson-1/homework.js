258403faaeebbac7da9e4309d72510ef8a7ead17
Alexander <sashka0264@yandex.ru>
2019-10-10 00:11:28 +0300

    Внес правки

--- ivan_petrichenko/lesson-1/homework.js
+++ ivan_petrichenko/lesson-1/homework.js
@@ -1,42 +1,29 @@
-const employers = ['Alex', '', 'ludmila', ' Viktor', '', 'oleg', 'iNna', 'Ivan', 'Alex', 'Olga', ' Ann'];
+const employers = ['Alex', '', 'ludmila', 'Viktor', '', ' oleg ', 'iNna  ', 'Ivan', 'Alex', 'Olga', ' Ann'];
 
-let employersNames = employers.filter( (name) => {
-    if (name.length > 0 && name.length != '') {
-		return name;
-	}
-});
-// Имена
-
-employersNames = employersNames.map( (name) => {
-	return name.toLowerCase().trim();
-});
-// Удаляем пробелы и заглавные буквы
+const employersNames = employers.filter(emp => emp.length > 0).map(emp => emp.toLowerCase().trim());
 
 const sponsors = {
     cash: [40000, 5000, 30400, 12000],
     eu: ['SRL', 'PLO', 'J&K'],
     rus: ['RusAuto', 'SBO']
 };
-console.log(employersNames);
+const {cash, eu, rus} = sponsors; 
+// // деструктуризация
 
-let calcCash = (own) => {
-    own = own || 0;
-    let everyCash = Array.prototype.slice.call(arguments);
-    let total = own;
-    for (let i = 0; i < everyCash[1].length; i++) {
-        total += +everyCash[1][i];
-    }
+const calcCash = (own = 0, everyCash) => {
+    const total = everyCash.reduce((accumulator, currentValue) => accumulator + currentValue, own);
     return total;
 }
 
-let money = calcCash(null, sponsors.cash);
+let money = calcCash(null, cash);
 
-let makeBusiness = (owner, director = 'Victor', cash, emp) => {
-    let sumSponsors = sponsors.eu.concat(sponsors.rus, 'unexpected sponsor');
-    console.log('We have a business. Owner: ' + owner + ', director: ' + director + '. Our budget: ' + cash + '. And our employers: ' +
-    emp);
+const makeBusiness = (owner, cash, emp, director = 'Victor') => {
+    const sumSponsors = [...rus, ...eu, 'unexpected sponsor'];
+    console.log(`We have a business. Owner: ${owner}, director: ${director}.`); 
+    console.log(`Our budget: ${money}. And our employers: ${emp}`);
     console.log('And we have a sponsors: ');
-    console.log.apply(null, sumSponsors);
-    console.log('Note. Be careful with ' + sponsors.eu[0] + ". It's a huge risk.");
+    console.log(...sumSponsors);
+    console.log(`Note. Be careful with ${eu[0]}. It's a huge risk.`);
 }
-makeBusiness.apply(null, ['Sam', null, money, employersNames]);
+makeBusiness(...['Sam',  money, employersNames] );
+
